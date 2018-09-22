af81d074de242532f210707b804ca5b7ba6451b8
Alvaro Monsalve <alvaromonsalve@yahoo.com.co>
2018-09-22 14:35:19 -0500

    add navbar and router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit, Renderer, Inject, ElementRef } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { DOCUMENT } from '@angular/platform-browser';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { Subscription } from 'rxjs';
+import { Location } from '@angular/common';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'a2mv-inmob-store';
+export class AppComponent implements OnInit {
+  private _router: Subscription;
+  @ViewChild(NavbarComponent) navbar: NavbarComponent;
+
+  constructor( private renderer: Renderer,
+    private router: Router,
+    @Inject(DOCUMENT, ) private document: any,
+    private element: ElementRef,
+    public location: Location) {}
+
+  ngOnInit() {
+    const navbar: HTMLElement = this.element.nativeElement.children[0].children[0];
+    this._router = this.router.events
+    .pipe(filter(event => event instanceof NavigationEnd))
+    .subscribe((event: NavigationEnd) => {
+      if (window.outerWidth > 991) {
+        window.document.children[0].scrollTop = 0;
+      } else {
+        window.document.activeElement.scrollTop = 0;
+      }
+      this.navbar.sidebarClose();
+      this.renderer.listenGlobal('window', 'scroll', ( _event ) => {
+        const number = window.scrollY;
+        let _location = this.location.path();
+        _location = _location.split('/')[2];
+
+        if (number > 150 || window.pageYOffset > 150) {
+            navbar.classList.remove('navbar-transparent');
+        } else if (_location !== 'login' && this.location.path() !== '/nucleoicons') {
+            // remove logic
+            navbar.classList.add('navbar-transparent');
+        }
+      });
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { RouterModule } from '@angular/router';
+import { routes } from './app.routing';
 
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserAnimationsModule,
+    NgbModule.forRoot(),
+    HttpClientModule,
+    RouterModule.forRoot( routes, {useHash: true} ),
   ],
   providers: [],
   bootstrap: [AppComponent]
